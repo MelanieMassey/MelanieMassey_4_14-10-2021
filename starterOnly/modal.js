3347a3992bf61dd99ecca5fa2e09c49359b5d2d5
Melanie Massey <nanie_d_enfer@hotmail.com>
2021-10-28 14:33:40 +0200

    issues #1 et début #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,5 +27,41 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+document.getElementById("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  validate();
+})
+
+function validate() {
+  const firstName = document.getElementById("first");
+    if(firstName.value.length < 2 || firstName.value.length == 0){
+    document.getElementById("first--error").style.display="inline-block";
+  }
+    else{
+      document.getElementById("first--error").style.display="none";
+    }
+}
+
+
+
+//Family Name - "minimum de 2 caractères / n'est pas vide"
+const familyName = document.getElementById("last");
+familyName.addEventListener("input",function(){
+  if(familyName.value.length < 2 || firstName.value.length == 0){
+    familyName.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    familyName.dispatchEvent(new Event("invalid"));
+  }
+})
+
+//Email - valide
+
+
+
+// NOTES COURS
+// element.addEventListener(nomEvenementAEcouter,fonctionAApeller)
+// preventDefault() - empeche execution evenement par defaut
+// checkVAlidity - vérifier si critères du champ sont valides avec true false
+// reportValidity - vérifier + message d'erreur
+// setCustomValidity("") - personnalise message erreur
 
 
